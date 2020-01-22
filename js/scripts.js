fc73ec92511bdfa48aede4da54fff5442caca040
Joseph Wangemann <fractalscape13@gmail.com>
2020-01-21 18:49:00 -0800

    minor styling changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var index = 0
   $("#main").click(function() {
-    var flavors = ['Naked Vanilla', 'Salted Caramel', 'Super Berry Swirl'];
+    var flavors = ['Chai & Eye', 'Salted Caramel', 'Superberry Swirl'];
     var currentFlavor = flavors[index];
     if (index < 3) {
       $("#flav").text(currentFlavor);
